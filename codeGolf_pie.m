93b2a9d9ca1865a26a26988975139396065540ba
Akshar Agarwal <aakshar2000@gmail.com>
2017-08-13 13:46:14 -0400

    Reduce bytes, fix input

--- codeGolf_pie.m
+++ codeGolf_pie.m
@@ -1,3 +1,4 @@
-eSt=num2str(exp(1));piSt=num2str(pi);pie=[]; for i=1:length(piSt) pie=[pie piSt(i) eSt(i)]; end
+n=input('n'); eSt=num2str(exp(1)); piSt=num2str(pi);pie=[]; for i=1:length(piSt) pie=[pie piSt(i) eSt(i)]; end pie(n)
 
 % https://codegolf.stackexchange.com/questions/138547/mix-pi-and-e-to-make-pie
+% Change pie to c, piSt to p, and eSt to e for less bytes
