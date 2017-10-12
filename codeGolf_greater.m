06a958baf428a1ce4216ace7d823f8a05798ae23
Akshar Agarwal <aakshar2000@gmail.com>
2017-10-12 18:42:14 -0400

    new golf

---
+++ codeGolf_greater.m
@@ -0,0 +1,19 @@
+a=input('')
+b=sprintf('%d', a)
+c=sort(b,'descend')
+
+
+% a=input('');
+% b=sprintf('%d',a) - '0';
+% c=[];
+% b=sort(b,'descend');
+% for i=1:size(b,2) 
+%     c(i)=b(i);
+% end
+% for y=1:size(c,2)
+%     e(y)=num2str(c(y));
+% end
+% % https://codegolf.stackexchange.com/questions/145118/i-am-greater-than-you/145121#145121
+% 
+
+% a=input('')
